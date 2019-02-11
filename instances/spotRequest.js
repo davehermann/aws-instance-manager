86942d1e8d1e9ebaa0845982fa07405e78432bb3
Dave Hermann <6787333+davehermann@users.noreply.github.com>
2019-02-11 12:10:44 -0500

    Add ability to define availability zone

--- instances/spotRequest.js
+++ instances/spotRequest.js
@@ -44,6 +44,103 @@ function selectConfiguration() {
     return inquirer.prompt(questions);
 }
 
+function selectAvailabilityZone(instanceConfiguration) {
+    const hoursBack = 48,
+        ec2 = new aws.EC2({ apiVersion: `2016-11-05`, }),
+        priceHistoryParams = {
+            InstanceTypes: [instanceConfiguration.LaunchSpecification.InstanceType],
+            StartTime: DateTime.utc().plus({ hours: -hoursBack }).toJSDate(),
+            EndTime: DateTime.utc().toJSDate()
+        };
+
+    console.log(JSON.stringify(priceHistoryParams));
+    return ec2.describeSpotPriceHistory(priceHistoryParams).promise()
+        .then(data => {
+            // Filter by ProductDescription - ask the user
+            let pricing = {};
+
+            data.SpotPriceHistory.forEach(price => {
+                if (!pricing[price.ProductDescription])
+                    pricing[price.ProductDescription] = {};
+
+                if (!pricing[price.ProductDescription][price.AvailabilityZone])
+                    pricing[price.ProductDescription][price.AvailabilityZone] = [];
+
+                pricing[price.ProductDescription][price.AvailabilityZone].push(price);
+            });
+
+            let names = [];
+            for (let desc in pricing)
+                names.push(desc);
+            names.sort();
+
+            let questions = [
+                {
+                    type: `list`,
+                    name: `instanceType`,
+                    message: `Select the instance type you're running to see pricing history for the last ${hoursBack} hours`,
+                    choices: names,
+                },
+            ];
+
+            return inquirer.prompt(questions)
+                .then(answers => {
+                    return pricing[answers.instanceType];
+                });
+        })
+        .then(instancePricing => {
+            // Sort zones by name
+            let zones = [];
+            for (let zone in instancePricing)
+                zones.push(zone);
+            zones.sort();
+
+            zones.forEach(zone => {
+                // Sort by time
+                instancePricing[zone].sort((a, b) => {
+                    return a.Timestamp.getTime() > b.Timestamp.getTime() ? -1 : 1;
+                });
+
+                // Show the price as hours/minutes ago
+                let zonePrices = [];
+                instancePricing[zone].forEach(price => {
+                    zonePrices.push(`${price.SpotPrice} (${DateTime.fromJSDate(price.Timestamp).toRelative({ unit: `hours` })})`);
+                });
+                console.log(`${zone}: ${zonePrices.join(` - `)}`);
+            });
+
+            return zones;
+        })
+        .then(zones => {
+            return ec2.describeSubnets({ Filters: [{ Name: `availability-zone`, Values: zones }] }).promise()
+                .then(data => {
+                    let mySubnets = data.Subnets.map(subnet => { return { zone: subnet.AvailabilityZone, subnet: subnet }; });
+
+                    let questions = [
+                        {
+                            type: `list`,
+                            name: `subnet`,
+                            message: `Available subnets to launch in:`,
+                            choices: mySubnets.map(subnet => { return subnet.zone; }).sort(),
+                        }
+                    ];
+
+                    return inquirer.prompt(questions)
+                        .then(answers => {
+                            let selectedSubnet = mySubnets.find(subnet => { return subnet.zone == answers.subnet; });
+
+                            return selectedSubnet.subnet;
+                        });
+                });
+        })
+        .then(launchSubnet => {
+            instanceConfiguration.LaunchSpecification.NetworkInterfaces[0].SubnetId = launchSubnet.SubnetId;
+        })
+        .then(() => {
+            return Promise.resolve(instanceConfiguration);
+        });
+}
+
 /**
  * Trigger a new EC2 spot request
  * @param {Object} answers - Answers to the spot configuration questions
@@ -52,21 +149,24 @@ function submitRequest(answers) {
     if (answers.selectedInstance == null)
         return Promise.resolve();
 
-    let ec2 = new aws.EC2({ apiVersion: `2016-11-05`, });
-
     // Copy the instance configuration
-    let launchInstance = JSON.parse(JSON.stringify(AvailableInstances[answers.selectedInstance]));
+    let instanceConfiguration = JSON.parse(JSON.stringify(AvailableInstances[answers.selectedInstance]));
 
-    // Add an expiration the requested hours in the future (2020-02-09T13:34:20Z)
-    if (answers.maximumLifetime > 0) {
-        const expirationTime = DateTime.utc().plus({ hours: +answers.maximumLifetime });
-        launchInstance.ValidUntil = expirationTime.toISO(); //`${expirationTime.toFormat(`yyyy-LL-dd`)}T${expirationTime.toFormat(`TT`)}Z`;
-    }
+    return selectAvailabilityZone(instanceConfiguration)
+        .then(launchInstance => {
+            const ec2 = new aws.EC2({ apiVersion: `2016-11-05`, });
 
-    if (answers.showRequest)
-        Info(launchInstance);
-
-    return ec2.requestSpotInstances(launchInstance).promise();
+            // Add an expiration the requested hours in the future (2020-02-09T13:34:20Z)
+            if (answers.maximumLifetime > 0) {
+                const expirationTime = DateTime.utc().plus({ hours: +answers.maximumLifetime });
+                launchInstance.ValidUntil = expirationTime.toISO(); //`${expirationTime.toFormat(`yyyy-LL-dd`)}T${expirationTime.toFormat(`TT`)}Z`;
+            }
+        
+            if (answers.showRequest)
+                Info(launchInstance);
+        
+            return ec2.requestSpotInstances(launchInstance).promise();
+        });
 }
 
 /**
